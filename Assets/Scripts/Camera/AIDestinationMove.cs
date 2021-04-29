fdc3b3c6f2a7a84e3a80aa2ba585968d66ec72db
Agustin Ferrari <agustin-ferrari@hotmail.com>
2021-04-29 20:02:11 +0200

    Start of Repository
    -This contains the basic settings for the repository and some basic scripts.

---
+++ Assets/Scripts/Camera/AIDestinationMove.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class AIDestinationMove : MonoBehaviour {
+    public Camera mainCam;
+    private bool trackMouse = false;
+    public Transform objectToMove;
+
+    private void Awake () {
+        if (!mainCam)
+            mainCam = Camera.main;
+    }
+
+    private void Update () {
+        //Toggle Tracking
+        if (Input.GetKeyDown(KeyCode.T))
+            trackMouse = !trackMouse;
+
+        //Perform Change
+        if (trackMouse) {
+            if (Physics.Raycast(mainCam.ScreenPointToRay(Input.mousePosition), out RaycastHit hit)) {
+                objectToMove.position = hit.point;
+            } 
+        }
+    }
+}
